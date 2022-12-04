0f410fbe2c6c7056a1baf3e87089993deaf8e3fa
Thymos8754 <8dlotxtb@anonaddy.me>
2022-12-04 15:06:38 -0700

    added push and pop, fixed insert

--- main.cpp
+++ main.cpp
@@ -8,13 +8,16 @@ class GrowableArray {
 private:
     int* data;
     int arraySize;
+    int capacity;
 
 public:
     GrowableArray() {
         data = (int*)malloc(sizeof(int) * ARRAY_START_SZ);
-        arraySize = ARRAY_START_SZ;
+        arraySize = 0;
+        capacity = ARRAY_START_SZ;
         for (int i = 0; i < 8; ++i) {
             (data)[i] = i;
+            arraySize +=1;
         }
 
         std::cout << std::endl;
@@ -31,23 +34,30 @@ public:
     }
 
     void insert(int num, int index) {
-        int *newArray = (int *) malloc(sizeof(int) * ARRAY_START_SZ);
-        bool pastIndex = false;
-        for(int i = 0; i < arraySize+1; i++ ){
+        capacity += 1;
+        int *newArray = (int *) malloc(sizeof(int) * capacity);
+        int newSize = 0;
+        bool passedIndex = false;
+        for(int i = 0; i < capacity; i++ ){
 
-            if(pastIndex){
+            if(passedIndex){
                 newArray[i] = data[i - 1];
+                newSize += 1;
             }
-            if(i != index && !pastIndex){
+            if(i != index && !passedIndex){
                 newArray[i] = data[i];
+                newSize += 1;
             } else {
-                if(!pastIndex){
-                    pastIndex = true;
+                if(!passedIndex){
+                    passedIndex = true;
                     newArray[i] = num;
+                    newSize += 1;
                 }
             }
         }
         data = newArray;
+        arraySize = newSize;
+        //free(data);
     }
 
     int lookup(unsigned int index) {
@@ -59,8 +69,43 @@ public:
 
         for(int i = 0; i < arraySize; i++ ){
             std::cout << data[i] << " ";
-            };
+            }
         }
+
+    int pop(){
+        int newSize = 0;
+        int *newArray = (int *) malloc(sizeof(int) * capacity);
+        for(int i = 0; i < capacity; i++ ){
+            if(i < arraySize - 1){
+                newArray[i] = data[i];
+                newSize += 1;
+            }
+            else{
+                int lastNum = data[i];
+                data = newArray;
+                arraySize = newSize;
+                return lastNum;
+            }
+        }
+    }
+
+    void push(int value){
+        int newSize = 0;
+        capacity += 1;
+        int *newArray = (int *) malloc(sizeof(int) * capacity);
+        for(int i = 0; i < capacity; i++ ){
+            if(i < arraySize) {
+                newArray[i] = data[i];
+                newSize += 1;
+            } else {
+                newArray[i] = value;
+                newSize +=1 ;
+                data = newArray;
+                arraySize = newSize;
+                break;
+            }
+        }
+    }
 };
 
 
@@ -70,5 +115,10 @@ int main() {
     std::cout << arr.lookup(2);
     arr.print();
     arr.insert(50, 4);
+    arr.print();
+    std::cout << "\n";
+    std::cout << arr.pop();
+    arr.print();
+    arr.push(1);
     arr.print();
 }
